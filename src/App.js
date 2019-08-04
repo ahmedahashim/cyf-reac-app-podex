bc7d6dcaf2109958ffe11ab2c3e00151f1b56707
ahmedahashim <ahmed.almadina@hotmail.com>
2019-08-04 16:20:45 +0100

    passing function as props on Logo component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import Logo from "./Logo"
 import BestPokemon from "./BestPokemon"
 import CaughtPokemon from "./CaughtPokemon"
 
-
+const logWhenClicked = () => {
+  alert("Hello World")
+}
 
 function App() {
+ 
   return (
     <div>
-      <Logo appName='Podex'/>
+      <Logo appName='Podex' onClickHandler={logWhenClicked}/>
       <BestPokemon pokemonNames= {['Squirtle', 'Bulbasaur', 'Charmander']}/>
       < CaughtPokemon date= {new Date().toLocaleDateString()}/>
     </div>
