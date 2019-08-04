d0d3dfa70f60d0aa6545e1858a54af988d8aebb7
ahmedahashim <ahmed.almadina@hotmail.com>
2019-08-04 15:42:22 +0100

    added class method on Logo component

--- src/Logo.js
+++ src/Logo.js
@@ -1,12 +1,15 @@
 import React, {Component} from 'react';
 
 class Logo extends Component {
+    logWhenClicked =()=>{
+        alert("Hello World")
+    }
     render(){
     return(
     
         <header>
             <h1>Welcome to the {this.props.appName}</h1>
-            <img src='https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png' alt="" />
+            <img onClick={this.logWhenClicked }src='https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png' alt="" />
         </header>
     )
 }};
